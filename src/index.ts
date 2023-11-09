1844b8bb757966b17e3031f9847a6bec6ebb7d9d
Morozov Mikhail <memphis.sail@gmail.com>
2023-11-09 21:58:41 +1000

    PROJECT:INIT

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { ajax } from 'rxjs/ajax';
+import {of, from, timer, range} from 'rxjs';
 
-const data$ = ajax.getJSON('https://api.github.com/search/repositories?q=rxjs');
+const o = range(0, 10)
 
-data$.subscribe((value) => console.log('data$ value', value));
-
-const dataGitLab$ = ajax.getJSON('https://gitlab.com/api/v4/projects?search=nodejs');
-
-dataGitLab$.subscribe((value) => console.log('dataGitLab$ value', value));
+o.subscribe({
+  next: (value: any) => console.log('Next:', value),
+  complete: () => console.log('Complete!'),
+  error: (error) => console.log('Error!', error)
+})
