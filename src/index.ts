f5b49f22bfadd1e73736c7d5c10b5fb3a9c4831e
Morozov Mikhail <memphis.sail@gmail.com>
2023-11-12 22:25:47 +1000

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import {of, from, timer, range} from 'rxjs';
+import { scheduled, from, asyncScheduler } from "rxjs";
+import { map } from "rxjs/operators";
+// import { ajax } from "rxjs/ajax";
 
-const o = range(0, 10)
+// const data$ = ajax.getJSON(
+// 	"https://api.github.com/search/repositories?q=prostohttp"
+// );
 
-o.subscribe({
-  next: (value: any) => console.log('Next:', value),
-  complete: () => console.log('Complete!'),
-  error: (error) => console.log('Error!', error)
-})
+// data$.subscribe((value: Partial<{[items:string]: any[]}>) => console.log("data$ value", value.items[0].full_name));
+
+const o = scheduled(from([1, 2, 3]), asyncScheduler).pipe(map((el) => el * 2));
+
+o.subscribe((x) => console.log(x));
